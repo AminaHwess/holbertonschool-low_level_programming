ba039d1873d5b8cb83879143182d1e1cb3864703
amina <amina.1111@yahoo.fr>
2023-10-16 20:37:44 +0100

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main-checks if positive
+ *Return: 0
+ */
+
+int main(void)
+{
+char ch = 'z';
+
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
