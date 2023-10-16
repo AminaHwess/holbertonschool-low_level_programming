80077a1be636c40b27c4ac76106a96815889eaab
amina <amina.1111@yahoo.fr>
2023-10-16 14:09:39 +0100

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int n;
+    int lastdigit;
 
 	srand(time(0));
 n = rand() - RAND_MAX / 2;
-int lastdigit;
 lastdigit = abs(n);
 while (lastdigit >= 10)
 { lastdigit = lastdigit % 10; }
