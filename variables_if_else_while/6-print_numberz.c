2ac8b7f9bec87da1b993d38a8314f3ffacf65261
amina <amina.1111@yahoo.fr>
2023-10-16 20:23:27 +0100

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-checks if positive
+ *Return: 0
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0 ; digit < 10 ; digit++)
+{putchar('0' + digit); }
+putchar('\n');
+
+
+return (0);
+}
