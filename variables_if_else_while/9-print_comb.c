fb849bea8deb02361dccdd51c3724543155b229d
amina <amina.1111@yahoo.fr>
2023-10-16 21:08:36 +0100

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main-checks if positive
+ *Return: 0
+ */
+int main(void)
+{
+char digit = 0;
+
+for (digit = 0 ; digit < 10 ; digit++)
+{putchar('0' + digit);
+if (digit < 9)
+{ putchar(','); }
+}
+
+
+putchar('\n');
+
+
+return (0);
+}
