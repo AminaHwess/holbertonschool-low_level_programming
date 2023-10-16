831a24bd03a06465e9a3aafc55be94f032932c24
amina <amina.1111@yahoo.fr>
2023-10-16 14:04:10 +0100

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ while (lastdigit >= 10)
 if (lastdigit > 5)
 {printf("Last digit of %d is %d and is greater than 5", n, lastdigit); }
 
-else if (lastdigit - 1 > 0 > 1)
+else if (lastdigit == 0)
 {printf("Last digit of %d is %d and is 0", n, lastdigit); }
 
 else
