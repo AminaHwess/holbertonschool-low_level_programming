28f7c0d3d6e7ad418b8a7f5ece2e03034d495616
amina <amina.1111@yahoo.fr>
2023-10-18 14:10:40 +0100

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - hours and minutes
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int m;
+	int k;
+
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 10; j++)
+{
+{
+for (m = 0; m < 5; m++)
+{
+{
+for (k = 0; k < 10; k++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(':');
+putchar('0' + m);
+putchar('0' + k);
+putchar('\n');
+}
+}
+}
+}
+}
+}
+return (0);
+}
