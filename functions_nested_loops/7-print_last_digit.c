ace19d7b5100ea3c476341b0226f8d3488511b99
amina <amina.1111@yahoo.fr>
2023-10-18 11:57:49 +0100

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - print_last_digit
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int d)
+{
+    int n;
+    n = d % 10;
+
+    while(n >= 10)
+    {n = n % 10;}
+
+    if (n >= 0)
+    {putchar(n + '0');}
+
+    else if (n < 0)
+    { n = n * -1;
+      putchar(n + '0');
+    }
+
+
+return (n) ;
+}
