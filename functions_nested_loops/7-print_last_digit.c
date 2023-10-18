7f84b5e11e8af3b99e43badceb0368ce3a6a9dea
amina <amina.1111@yahoo.fr>
2023-10-18 12:08:56 +0100

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * main - print_last_digit
- *
+ * print_last_digit - entry point
+ *@d : integer
  * Return: Always 0.
  */
 
 int print_last_digit(int d)
 {
-	int n;
-	n = d % 10;
+int n;
+n = d % 10;
 
-	while (n >= 10)
-	{
-		n = n % 10;
-	}
+while (n >= 10)
+{
+n = n % 10;
+}
 
-	if (n <= 0)
-	{
-		n = n * -1;
-	}
-	_putchar(n + '0');
-	return (n);
+if (n <= 0)
+{
+n = n * -1;
+}
+_putchar(n + '0');
+return (n);
 }
