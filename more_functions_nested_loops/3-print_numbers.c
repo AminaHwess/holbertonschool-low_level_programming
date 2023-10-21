2d5aefeb96b6e8b29ba955bda7411e41d1857295
amina <amina.1111@yahoo.fr>
2023-10-21 14:10:43 +0100

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - check for a digit
+ *@c : integer
+ * Return: Always 0.
+ */
+
+void print_numbers(int c)
+
+{
+
+for (c = 0 ; c <= 9 ; c++)
+
+{
+putchar('0' + c);
+putchar('\n');
+}
+
+return (0);
+}
