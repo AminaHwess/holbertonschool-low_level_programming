250a7d18f6c409f5f3cc7389b2315e4300fdd7a8
amina <amina.1111@yahoo.fr>
2023-10-21 12:57:18 +0100

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * isalpha - check for a digit
+ *@c : integer
+ * Return: Always 0.
+ */
+int _isdigit(int c);
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+
+}
+
+else
+{
+
+return(0);
+
+}
+}
