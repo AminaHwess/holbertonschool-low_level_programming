d2fcbeaff177b5aa465c53da41a0b65903531e78
amina <amina.1111@yahoo.fr>
2023-10-21 15:29:12 +0100

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 void print_most_numbers(void)
 
 {
-    int f;
-    
-    for (f = 0; f <=9; f++ )
+int f;
+
+for (f = 0; f <= 9; f++)
 {
-        if(f != 2 && f !=  4)
-    {_putchar(f + '0');
-    }
-    }
-     _putchar('\n');
+if (f != 2 && f !=  4)
+{_putchar (f + '0');
+}
+}
+_putchar ('\n');
 }
