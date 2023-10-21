c2f2337e788e90e5f5bcca4e52d6f9d4e167f225
amina <amina.1111@yahoo.fr>
2023-10-21 15:50:28 +0100

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,21 @@
 #include "main.h"
- /**
- * print_most_numbers : function
- * Description: function that prints numbers except 2 and 4
- * Return: void (Success) .
- */
 
+/**
+ * print_most_numbers - Entry point
+ *
+ *function that prints numbers except 2 and 4
+ * Return: Always 0.
+ **/
 void print_most_numbers(void)
-
 {
-int f;
+	int f;
 
-for (f = 0; f <= 9; f++)
-{
-if (f != 2 && f !=  4)
-{_putchar (f + '0');
-}
-}
-_putchar ('\n');
+	for (f = 0; f <= 9; f++)
+	{
+		if (f != 2 && f != 4)
+		{
+			_putchar(f + '0');
+		}
+	}
+	_putchar('\n');
 }
