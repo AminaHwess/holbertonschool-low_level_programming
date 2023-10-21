b12dfe57ee60637cc2b6f57df5f70f7f66a620ad
amina <amina.1111@yahoo.fr>
2023-10-21 15:26:10 +0100

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+ /**
+ * print_most_numbers - function
+ * Description: function that prints numbers except 2 and 4
+ * Return: void (Success) .
+ */
+
+void print_most_numbers(void)
+
+{
+    int f;
+    
+    for (f = 0; f <=9; f++ )
+{
+        if(f != 2 && f !=  4)
+    {_putchar(f + '0');
+    }
+    }
+     _putchar('\n');
+}
