39c136e761ffe6da8b7486d140e1e9d174d6ce71
amina <amina.1111@yahoo.fr>
2023-10-21 14:49:54 +0100

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,19 @@
 
 /**
  * print_numbers - check for a digit
- *@c : integer
+ * function that checks for numbers
  * Return: Always 0.
  */
 
-void print_numbers(int c)
+void print_numbers(void)
 {
 
+int c;
 
-for (c = 0 ; c <= 9 ; c++)
+for (c = 0; c <= 9; c++)
 
 {
-_putchar(c + '0');
+_putchar('0' + c);
 }
 
 _putchar('\n');
