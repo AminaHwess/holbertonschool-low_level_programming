0bd676ebbb1ee37901fd5ed92608c7f506ad36e9
amina <amina.1111@yahoo.fr>
2023-10-21 12:11:33 +0100

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ *@c : integer
+ * Return: void (Success)
+ */
+
+
+
+
+
+int _isupper(int c);
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
