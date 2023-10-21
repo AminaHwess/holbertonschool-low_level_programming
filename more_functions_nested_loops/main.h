b2cc34414ab6d0815f538d510a0f5ec7da0b2f3a
amina <amina.1111@yahoo.fr>
2023-10-21 12:28:50 +0100

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,24 @@
 #include<stdio.h>
 int _isupper(int c);
+
+#include "main.h"
+
+/**
+ *isupper - Entry point
+ *@c : integer
+ *Return: void (Success)
+*/
+
+int _isupper(int c);
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
