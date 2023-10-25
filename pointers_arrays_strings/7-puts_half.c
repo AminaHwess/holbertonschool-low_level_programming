d1e7674ec5b42934d2568a80ba73490dac9c4b0c
amina <amina.1111@yahoo.fr>
2023-10-25 12:25:37 +0100

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - check the code
+ * @str : char
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+
+	int i = strlen(str);
+	int j;
+
+	for (j = i / 2; i > j; j++)
+	{
+		_putchar(str[j]);
+
+	}
+	_putchar('\n');
+}
