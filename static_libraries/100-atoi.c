74ea8ca4344dba011a2096b91f84859ca93538aa
amina <amina.1111@yahoo.fr>
2023-11-02 10:34:12 +0100

    100-atoi.c

--- static_libraries/100-atoi.c
+++ static_libraries/100-atoi.c
@@ -1,56 +1,43 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 /**
- * removeChar -a function that removes a character
- * from a string
- * @str: the string
- * @c: the character
- * Return: void
- */
-void removeChar(char *str, char c)
-{
-	int i, j;
-	int len = strlen(str);
-
-	for (i = j = 0; i < len; i++)
-	{
-		if (str[i] != c)
-		{
-			str[j++] = str[i];
-		}
-	}
-	str[j] = '\0';
-}
-/**
- * _atoi -a function that convert a string to an integer.
- * @s: the string
- * Return: int value
+ * _atoi - check the code
+ * @s : char
+ * Return: Always 0.
  */
 int _atoi(char *s)
 {
-	int a;
-	int i;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int foundnum = 0;
 
-	i = 0;
-	a = 1;
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 		{
-			a = a * -1;
+			sign *= -1;
 		}
-		i++;
-	}
-	i = 0;
-	while (*(s + i) != '\0')
-	{
-		if (s[i] == '-' || s[i] == '+')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			removeChar(s, s[i]);
+			if (sign == -1 && result * 10 == 2147483640 && s[i] == '8')
+			{
+				result = -2147483648;
+				return (result);
+			}
+			else
+			{
+				result = result * 10 + (s[i] - '0');
+				foundnum = 1;
+			}
+		}
+
+		else if (foundnum)
+		{
+			break;
 		}
 		i++;
 	}
-	return (atoi(s) * a);
+	result *= sign;
+	return (result);
 }
