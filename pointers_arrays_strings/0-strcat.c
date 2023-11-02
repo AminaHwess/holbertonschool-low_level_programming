24f4810562d359de05c3ac74e81ad6248408cad0
amina <amina.1111@yahoo.fr>
2023-11-02 10:39:04 +0100

    pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -21,4 +21,4 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[j] = '\0';
 	return (dest);
-}
+	}
---
+++ pointers_arrays_strings/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - function that concatenates two strings.
+ * @src : string
+ * @dest : string
+ * Return: dest.
+ */
+
+int main(void)
+{
+printf("hala amina\n ");
+return(1);
+}
