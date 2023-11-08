7798176c0f899e02cb1f7f4245fbc4b18ecf4d44
amina <amina.1111@yahoo.fr>
2023-11-08 10:50:19 +0100

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - check the code
+ *@str : string
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+
+	int i;
+	char *s;
+	int n = strlen(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(char) * (n + 1));
+
+	if (str != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			s[i] = str[i];
+		}
+	}
+
+	else
+	{
+		return (NULL);
+	}
+
+	s[i] = '\0';
+	return (s);
+}
