6d0f1c7195d1840cc10b94fdbb79c9f974863e83
amina <amina.1111@yahoo.fr>
2023-11-11 10:53:35 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,37 +4,46 @@
 #include <string.h>
 
 /**
- * main - check the code
- *
+ * str_concat  - concatenate 2 strings
+ *@s1 : string 1
+ *@s2 : string 2
  * Return: Always 0.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-int i;
-int j;
-char *p;
-int a= 0;
-int b= 0;
-if (s1 != NULL){
-     a=strlen(s1);
-}
-if (s2 != NULL){
-    b=strlen(s2);
-}
+	int i;
+	int j;
+	char *p;
+	int a = 0;
+	int b = 0;
 
-p = malloc((a+b+1) * sizeof(char));
+	if (s1 != NULL)
+	{
+		a = strlen(s1);
+	}
+	if (s2 != NULL)
+	{
+		b = strlen(s2);
+	}
 
+	p = malloc((a + b + 1) * sizeof(char));
 
-if(p==NULL)
-{return(NULL);}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 
-for(i=0; i<a; i++)
-{p[i] = s1[i];}
+	for (i = 0; i < a; i++)
+	{
+		p[i] = s1[i];
+	}
 
-for(j=0; j<b; j++)
-{p[i] = s2[j];
-i++;}
-p[i]= '\0';
-return(p);
+	for (j = 0; j < b; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
 }
