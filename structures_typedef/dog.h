224b6b565a34ad2cc410b2e32badf4f86103ba48
amina <amina.1111@yahoo.fr>
2023-11-13 09:48:40 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+/**
+ * struct dog - check the code
+ *@name : char
+ *@age : float
+ *@owner : char
+ * Return: Always 0.
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
