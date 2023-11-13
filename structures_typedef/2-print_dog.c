3cda92d89823d4796218772664d59d9b67855aa5
amina <amina.1111@yahoo.fr>
2023-11-13 13:26:22 +0100

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,14 +19,13 @@ void print_dog(struct dog *d)
 				printf("Name:(nil)\n");
 			}
 
-			if (d->owner == NULL)
-			{
-				printf("Owner:(nil)\n");
-			}
-
 			if (d->age == 0)
 			{
 				printf("Age: %f\n", d->age);
+			}
+			if (d->owner == NULL)
+			{
+				printf("Owner:(nil)\n");
 			}
 		}
 
