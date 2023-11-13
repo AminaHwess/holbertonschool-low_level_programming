0240314a74fa28d2cd95c9bf7e30b47d1c04da6f
amina <amina.1111@yahoo.fr>
2023-11-13 12:57:20 +0100

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - check the code
+ * @d : struct
+ * Return: void.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+
+	if (d->name == NULL)
+	{
+		printf("Name:(nil)\n");
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("Owner:(nil)\n");
+	}
+
+	if (d->age == 0)
+	{
+		printf("Age: %f\n", d->age);
+	}
+}
