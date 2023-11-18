a38a9b988b1b2cd58ead716dc1775297849da5ae
amina <amina.1111@yahoo.fr>
2023-11-18 22:22:19 +0100

    3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -32,7 +32,6 @@ int op_sub(int a, int b);
  */
 
 int op_mul(int a, int b);
-
 {
 	return (a * b);
 }
@@ -45,7 +44,6 @@ int op_mul(int a, int b);
  */
 
 int op_div(int a, int b);
-
 {
 		return (a / b);
 }
