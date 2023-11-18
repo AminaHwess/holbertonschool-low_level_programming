5e7e9ba93d69986a4906f7b47eab90a17510fdda
amina <amina.1111@yahoo.fr>
2023-11-18 17:45:00 +0100

    function pointers

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,27 +1,33 @@
 #include "3-calc.h"
 
+/**
+ * get_op_func - check the code
+ * @s : char
+ * Return: null
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}};
-    int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i;
 
-    i = 0;
+	i = 0;
 
-    while (i < 6)
+	while (i < 6)
 
-    {
-        if (strcmp(s, ops[i].op))
+	{
+		if (strcmp(s, ops[i].op))
 
-        {
-            return ((*f));
-        }
-        i++;
-    }
-    return (NULL);
+		{
+			return ((*f));
+		}
+		i++;
+	}
+	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,33 +1,41 @@
 #include "3-calc.h"
 
+/**
+ * main - check the code
+ * @argc : int
+ * @argv : char
+ * Return: 0.
+ */
+
 int main(int argc, char *argv[])
 
 {
-    int a, b;
+	int a;
+	int b;
 
-    if (argc == 4)
+	if (argc == 4)
 
-    {
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
 
-        if ((*get_op_func(argv[2]) == NULL))
-        {
-            printf("Error\n");
-            exit(99);
-        }
+		if (*get_op_func(argv[2]) == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
 
-        else
-        {
+		else
+		{
 
-            printf(("%d\n" * get_op_func(argv[2]))(a, b));
-        }
-    }
+			printf(("%d\n" * get_op_func(argv[2]))(a, b));
+		}
+	}
 
-    else
-    {
-        printf("Error");
-        exit(98);
-    }
-    return (0);
+	else
+	{
+		printf("Error");
+		exit(98);
+	}
+	return (0);
 }
