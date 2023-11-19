c902b79dea5fcc8dc469c25d2ba0b58fa5c1d41e
amina <amina.1111@yahoo.fr>
2023-11-19 23:03:46 +0100

    2-orint_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,43 +3,53 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - check the code
+ * print_strings - check the code
+ * @separator : const char
  * @n : const unsigned int
- * Return: sum.
+ * Return: string.
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
+	unsigned int i;
 
-    va_list args;
-    va_start(args, n);
+	va_list args;
 
-    for (i = 0; i < n; i++)
+	va_start(args, n);
 
-    {
+	for (i = 0; i < n; i++)
 
-        printf("%s", strnl(va_arg(args, char *)));
+	{
 
-        if (separator != NULL && i < n - 1)
-        {
-            printf("%s", separator);
-        }
-    }
-    va_end(args);
-    printf("\n");
+		printf("%s", strnl(va_arg(args, char *)));
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
 }
+
+
+/**
+ * strnl - check the code
+ * @s :  char
+ * Return: s.
+ */
 
 char *strnl(char *s)
 
 {
-    char *n = "(nil)";
-    if (s == NULL)
-    {
-        return (n);
-    }
-    else
-    {
-        return (s);
-    }
+	char *n = "(nil)";
+
+	if (s == NULL)
+	{
+		return (n);
+	}
+	else
+	{
+		return (s);
+	}
 }
