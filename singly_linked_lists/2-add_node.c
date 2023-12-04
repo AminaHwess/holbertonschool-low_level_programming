51bdc9898afd2e34655d9f92c62f00f4ddae2d29
amina <amina.1111@yahoo.fr>
2023-12-04 16:38:21 +0100

    2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - check the code
+ * @head: ptr
+ * @str: const char
+ * Return: Always 0.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *firstnode = malloc(sizeof(list_t));
+
+	if (firstnode == NULL)
+	{
+		return (NULL);
+	}
+
+	firstnode->str = strdup(str);
+
+	if (firstnode->str == NULL)
+	{
+		return (NULL);
+	}
+
+	firstnode->len = strlen(firstnode->str);
+	firstnode->next = *head;
+	*head = firstnode;
+	return (firstnode);
+}
