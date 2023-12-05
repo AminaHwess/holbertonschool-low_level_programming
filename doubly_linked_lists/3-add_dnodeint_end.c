1b3b6326cda312e0107eb6d23b8d46405aaf2d4b
amina <amina.1111@yahoo.fr>
2023-12-05 14:05:17 +0100

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,8 +16,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (lastnode == NULL)
 	{
-		lastnode -> prev = NULL;
-		lastnode -> next = NULL;
 		free(lastnode);
 		return (NULL);
 	}
@@ -27,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!*head)
 	{
 		*head = lastnode;
-		lastnode->next = NULL;
+		lastnode->prev = NULL;
 	}
 
 	else
