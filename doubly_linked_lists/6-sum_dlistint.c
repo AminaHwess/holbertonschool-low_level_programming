51fb4594d0f7299458e74583b7059fe16af7cdde
amina <amina.1111@yahoo.fr>
2023-12-06 23:06:52 +0100

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - check the code
+ * @head: ptr
+ * Return: sum.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+	int sum = 0;
+
+	ptr = head;
+
+	if (!ptr)
+	{
+		return (0);
+	}
+
+	else
+	{
+
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+			sum += ptr->n;
+		}
+	}
+
+	return (sum);
+}
