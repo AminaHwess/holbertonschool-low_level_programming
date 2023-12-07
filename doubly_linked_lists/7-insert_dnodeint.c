8160dd7d2e69089c61ae6c47da96c330b2c4c037
azizios <hdriaziz699@gmail.com>
2023-12-07 17:31:30 +0100

    7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *node = malloc(sizeof(dlistint_t)), *tmp = *h;
+    unsigned int i = 1;
+    if (!node)
+        return (0);
+    node->n = n;
+    if (idx == 0)
+    {
+        node->prev = NULL;
+        node->next = *h;
+        *h = node;
+    }
+
+    while (i < idx)
+    {
+        tmp = tmp->next;
+        if (tmp == NULL)
+        {
+            return (NULL);
+        }
+        i++;
+    }
+    if (tmp->next != NULL)
+    {
+        node->next = tmp->next;
+        tmp->next->prev = node;
+        tmp->next = node;
+        node->prev = tmp;
+    }
+    else if (tmp->next == NULL)
+    {
+        node->prev = tmp;
+        tmp->next = node;
+        node->next = NULL;
+    }
+    return (node);
+}
