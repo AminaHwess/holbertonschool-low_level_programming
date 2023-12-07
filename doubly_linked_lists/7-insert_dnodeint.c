464f859ad2d7bb19fa2751f1023ee9c46d026b83
azizios <hdriaziz699@gmail.com>
2023-12-07 17:50:13 +0100

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     {
         *h = node;
         node->prev = NULL;
-        node->next = *h;
+        node->next = NULL;
     }
 
     while (i < idx && idx > 0)
