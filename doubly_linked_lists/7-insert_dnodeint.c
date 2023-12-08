59c1a62f88f2a8c1386c665895f1b0476415d8b3
amina <amina.1111@yahoo.fr>
2023-12-08 14:07:15 +0100

    insert dnode

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,8 +26,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     {
         node->prev = NULL;
         node->next = *h;
+        (*h)->prev = node;
         *h = node;
-        return(node);
+        return (node);
     }
 
     while (i < idx)
