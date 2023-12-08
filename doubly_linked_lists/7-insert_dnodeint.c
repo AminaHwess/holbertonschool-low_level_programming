a181a0a4ac6366d0557783bbdc82fb1223aef48e
amina <amina.1111@yahoo.fr>
2023-12-08 14:24:01 +0100

    insert dnode

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,47 +11,46 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *node = malloc(sizeof(dlistint_t)), *tmp = *h;
-    unsigned int i = 1;
-    if (!node)
-        return (0);
-    node->n = n;
-    if (!*h)
-    {
-        *h = node;
-        node->prev = NULL;
-        node->next = NULL;
-        return(node);
-    }
-    if (idx == 0 && h)
-    {
-        node->prev = NULL;
-        node->next = *h;
-        (*h)->prev = node;
-        *h = node;
-        return (node);
-    }
+	dlistint_t *node = malloc(sizeof(dlistint_t)), *tmp = *h;
+	unsigned int i = 1;
 
-    while (i < idx)
-    {
-        tmp = tmp->next;
-        if (tmp == NULL)
-            return (NULL);
-        i++;
-    }
-    if (tmp->next != NULL)
-    {
-        node->next = tmp->next;
-        node->next->prev = node;
-        tmp->next = node;
-        node->prev = tmp;
-    }
-    if (tmp->next == NULL)
-    {
-        node->prev = tmp;
-        tmp->next = node;
-        node->next = NULL;
-        *h = tmp;
-    }
-    return (node);
+	if (!node)
+		return (0);
+
+	node->n = n;
+	if (!*h)
+		{
+		*h = node;
+		return (node);
+	}
+	if (idx == 0 && h)
+	{
+		node->prev = NULL;
+		node->next = *h;
+		(*h)->prev = node;
+		*h = node;
+		return (node);
+	}
+
+	while (i < idx)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+		i++;
+	}
+	if (tmp->next == NULL)
+	{
+		node->prev = tmp;
+		tmp->next = node;
+		node->next = NULL;
+	}
+	if (tmp->next != NULL)
+	{
+		node->next = tmp->next;
+		node->next->prev = node;
+		tmp->next = node;
+		node->prev = tmp;
+	}
+	return (node);
 }
