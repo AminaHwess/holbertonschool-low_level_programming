7ed5f8d0b183b1d703d87964fb5b0a9672add61c
amina <amina.1111@yahoo.fr>
2023-12-10 19:33:53 +0100

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - check the code
+ * @filename: ptr
+ * @letters: nbr of letters
+ * Return: sz.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	char buff[50000];
+	int fd, sz;
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+	sz = read(fd, buff, letters);
+	write(1, buff, sz);
+
+	if (sz == -1)
+	{
+		return (0);
+	}
+
+	close(fd);
+
+	return (sz);
+}
